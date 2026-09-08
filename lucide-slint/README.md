315a315dd3fe1115b3982cfc245beb2e33a61c1d
Lance Hu <2124757129@qq.com>
2026-09-08 11:00:38 +0800

    docs: update

--- lucide-slint/README.md
+++ lucide-slint/README.md
@@ -8,3 +8,9 @@ Use lucide icons in your Slint applications with ease!
 ## [Documentation](https://github.com/cnlancehu/lucide-slint)
 
 Please refer to [README](https://github.com/cnlancehu/lucide-slint)
+
+## [Detailed tutorial](https://lance.hk/p/displaying-icons-in-slint/)
+
+For a deep dive into Lucide Slint's internals, interactive examples and more explanation, I have [a post](https://lance.hk/p/displaying-icons-in-slint/) all about it!
+
+There are also [some tips](https://lance.hk/p/displaying-icons-in-slint/#tips) that might be helpful.
--- lucide-slint/src/lib.rs
+++ lucide-slint/src/lib.rs
@@ -8,6 +8,12 @@
 //! ## [Documentation](https://github.com/cnlancehu/lucide-slint)
 //!
 //! Please refer to [README](https://github.com/cnlancehu/lucide-slint)
+//!
+//! ## [Detailed tutorial](https://lance.hk/p/displaying-icons-in-slint/)
+//!
+//! For a deep dive into Lucide Slint's internals, interactive examples and more explanation, I have [a post](https://lance.hk/p/displaying-icons-in-slint/) all about it!
+//!
+//! There are also [some tips](https://lance.hk/p/displaying-icons-in-slint/#tips) that might be helpful.
 
 /// Returns the file path to the `lucide.slint` file included in this crate.
 ///
